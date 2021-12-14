7e7d5e50088e3735ae42e73e8fbc4059a5fef1c5
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-14 12:35:05 -0400

    Fix js linter error

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
-const mobilMenu = function () {
-const mobileMenu = document.querySelector('#mobile-links');
-const links = document.querySelectorAll('#mobile-links li');
-const activeLinks = document.querySelectorAll('.menu-links');
-const toggleBtn = document.querySelector('.mobile-hamburguer');
+  const mobilMenu = function () {
+  const mobileMenu = document.querySelector('#mobile-links');
+  const links = document.querySelectorAll('#mobile-links li');
+  const activeLinks = document.querySelectorAll('.menu-links');
+  const toggleBtn = document.querySelector('.mobile-hamburguer');
 
-toggleBtn.addEventListener('click', () => {
-  mobileMenu.classList.toggle('mobile-menu-link');
-  activeLinks.forEach((link) => {
-    link.classList.toggle('links-active');
-  });
-});
-
-links.forEach((link) => {
-  link.addEventListener('click', () => {
+  toggleBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('mobile-menu-link');
     activeLinks.forEach((link) => {
       link.classList.toggle('links-active');
     });
-    toggleBtn.classList.toggle('active');
   });
-});
-
-document.addEventListener('keydown', (e) => {
-  if(e.key === 'Tab') {
-    mobileMenu.classList.toggle('mobile-menu-link');
-    activeLinks.forEach( (link) => {
-      link.classList.toggle('links-active');
+  
+  links.forEach((link) => {
+    link.addEventListener('click', () => {
+      mobileMenu.classList.toggle('mobile-menu-link');
+      activeLinks.forEach((link) => {
+        link.classList.toggle('links-active');
+      });
+      toggleBtn.classList.toggle('active');
     });
-  }
+  });
+  document.addEventListener('keydown', (e) => {
+    if(e.key === 'Tab') {
+      mobileMenu.classList.toggle('mobile-menu-link');
+      activeLinks.forEach( (link) => {
+        link.classList.toggle('links-active');
+      });}
 });
 
 $(document).ready(function(){
