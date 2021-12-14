046ca6407fa35b3af534e53ecc2709f0a55b4328
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-14 12:24:54 -0400

    fix linter errors

--- main.js
+++ main.js
@@ -1,43 +1,40 @@
-  const mobilMenu = function () {
-  const mobileMenu = document.querySelector("#mobile-links");
-  const links = document.querySelectorAll("#mobile-links li");
-  const activeLinks = document.querySelectorAll(".menu-links");
-  const toggleBtn = document.querySelector(".mobile-hamburguer");
-  
-  toggleBtn.addEventListener("click", () => {
-    mobileMenu.classList.toggle("mobile-menu-link");
+const mobilMenu = function () {
+const mobileMenu = document.querySelector('#mobile-links');
+const links = document.querySelectorAll('#mobile-links li');
+const activeLinks = document.querySelectorAll('.menu-links');
+const toggleBtn = document.querySelector('.mobile-hamburguer');
+
+toggleBtn.addEventListener('click', () => {
+  mobileMenu.classList.toggle('mobile-menu-link');
+  activeLinks.forEach((link) => {
+    link.classList.toggle('links-active');
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener('click', () => {
+    mobileMenu.classList.toggle('mobile-menu-link');
     activeLinks.forEach((link) => {
-      link.classList.toggle("links-active");
+      link.classList.toggle('links-active');
     });
+    toggleBtn.classList.toggle('active');
   });
+});
 
-  links.forEach((link) => {
-    link.addEventListener("click", () => {
-
-      mobileMenu.classList.toggle("mobile-menu-link");
-      activeLinks.forEach((link) => {
-        link.classList.toggle("links-active");
-      });
-      
-      toggleBtn.classList.toggle('active');
-    }); 
-  });
-
-  document.addEventListener("keydown", (e) => {
-    if(e.key === "Tab") {
-      mobileMenu.classList.toggle("mobile-menu-link");
-
-      activeLinks.forEach( (link) => {
-        link.classList.toggle("links-active");
-      });
-    }
-  });
-
-  $(document).ready(function(){
-    $(".mobile-hamburguer").click(function(){
-      $(".mobile-hamburguer").toggleClass("active")
+document.addEventListener('keydown', (e) => {
+  if(e.key === 'Tab') {
+    mobileMenu.classList.toggle('mobile-menu-link');
+    activeLinks.forEach( (link) => {
+      link.classList.toggle('links-active');
     });
+  }
+});
+
+$(document).ready(function(){
+  $('.mobile-hamburguer').click(function(){
+    $('.mobile-hamburguer').toggleClass('active')
   });
+});
 };
 
 mobilMenu();
