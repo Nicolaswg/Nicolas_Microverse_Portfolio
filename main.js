d8cd01053ef2dab32816f24fce532906575e7d7e
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-13 21:38:03 -0400

    Fix new mobile menu links display

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 const mobilMenu = function () {
   const mobileMenu = document.querySelector("#mobile-links");
   const links = document.querySelectorAll("#mobile-links li a");
-  const activeLinks = document.querySelectorAll(".links-active");
+  const activeLinks = document.querySelectorAll(".menu-links");
   const toggleBtn = document.querySelector(".mobile-hamburguer");
   
   toggleBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("mobile-menu-link");
+    activeLinks.forEach((link) => {
+      link.classList.toggle("links-active");
+    });
   });
 
   links.forEach((link) => {
