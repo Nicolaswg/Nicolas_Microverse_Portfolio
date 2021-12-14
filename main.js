865d74ab07e5d819a2ff6f593803096d26fa3ac1
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-13 20:53:48 -0400

    Fix menu link

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const mobilMenu = function () {
   const mobileMenu = document.querySelector("#mobile-links");
   const links = document.querySelectorAll("#mobile-links li a");
+  const activeLinks = document.querySelectorAll(".links-active");
   const toggleBtn = document.querySelector(".mobile-hamburguer");
-
+  
   toggleBtn.addEventListener("click", () => {
     mobileMenu.classList.toggle("mobile-menu-link");
   });
