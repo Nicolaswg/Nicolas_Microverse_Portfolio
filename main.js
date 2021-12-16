535f550802ede4361f464103012ba827da83c6b0
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-15 21:01:35 -0400

    Fix linter errors v1.2

--- main.js
+++ main.js
@@ -57,77 +57,76 @@ const mobilMenu = function menu() {
 
 // object element actribute generator
 const addAttributes = (element, attrObj) => {
-  for (let attr in attrObj) {
-    if (attrObj.hasOwnProperty(attr)) element.setAttribute(attr, attrObj[attr]);
-  }
-};
-// HTML and chidl generator
-const createCustomElement = (element,attributes,children) => {
-  let customElement = document.createElement(element);
-  if (children !== undefined) children.forEach(el => {
-    if (el.nodeType) {
-      if (el.nodeType === 1 || el.nodeType === 11) customElement.appendChild(el);
-    } else {
-      customElement.innerHTML += el;
+  Object.keys(attrObj).forEach((attr) => {
+    if (Object.prototype.hasOwnProperty.call(attrObj, attr)) {
+      element.setAttribute(attr, attrObj[attr]);
     }
   });
-  addAttributes(customElement,attributes);
+};
+// HTML and chidl generator
+const createCustomElement = (element, attributes, children) => {
+  const customElement = document.createElement(element);
+  if (children !== undefined) {
+    children.forEach((el) => {
+      if (el.nodeType) {
+        if (el.nodeType === 1 || el.nodeType === 11) customElement.appendChild(el);
+      } else {
+        customElement.innerHTML += el;
+      }
+    });
+  }
+  addAttributes(customElement, attributes);
   return customElement;
 };
 // Print modal-bg and modal content containers
-const printModal = content => {
-  const modalContent = createCustomElement('div',{
+const printModal = (content) => {
+  const modalContent = createCustomElement('div', {
     id: 'modal-content',
     class: 'modal',
-  },[content]);
-  
-  const modalContainer = createCustomElement('div',{
+  }, [content]);
+
+  const modalContainer = createCustomElement('div', {
     id: 'modal-container',
     class: 'modal-bg',
-  },[modalContent]);
-  // Show modal in the dom  
+  }, [modalContent]);
+
   document.body.appendChild(modalContainer);
-  // remmove modal from dom
-  const removeModal = () => {
-    document.body.removeChild(modalContainer);
-  }
-}
+};
 
-const modalProject =[
+const modalProject = [
   {
-  title: 'Tonic',
-  img: 'src/modal-img-a.png',
-  description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
-  pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
-  sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
-},
-{
-  title: 'Multi-Post Stories',
-  img: 'src/portfolio_img_b.PNG',
-  description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
-  pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
-  sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
-},
-
-{
-  title: 'Tonic',
-  img: 'src/portfolio_img_c.PNG',
-  description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
-  pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
-  sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
-},
-{
-  title: 'Multi-Post Stories',
-  img: 'src/portfolio_img_d.PNG',
-  description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
-  pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
-  sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
-}]
+    title: 'Tonic',
+    img: 'src/modal-img-a.png',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
+    pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
+    sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
+  },
+  {
+    title: 'Multi-Post Stories',
+    img: 'src/portfolio_img_b.PNG',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
+    pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
+    sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
+  },
+  {
+    title: 'Tonic',
+    img: 'src/portfolio_img_c.PNG',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
+    pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
+    sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
+  },
+  {
+    title: 'Multi-Post Stories',
+    img: 'src/portfolio_img_d.PNG',
+    description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus inventore nulla doloribus quia! Ea dolor, sequi reprehenderit architecto eum magnam molestiae dolore quae officia, minus voluptatum animi reiciendis qui illo ipsam nisi soluta culpa? Incidunt, minus, eaque dolorem doloremque enim tempore ipsum debitis dignissimos recusandae laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem laborum sit harum sequi quibusdam provident officia nam odit vitae illum magni laudantium soluta voluptatem.',
+    pageLink: 'https://nicolaswg.github.io/Nicolas_Microverse_Portfolio/',
+    sourcesLink: 'https://github.com/Nicolaswg/Nicolas_Microverse_Portfolio',
+  }];
 // Modal projects event
 // First Modal
-projectBtn.forEach((btn,index) => {
+projectBtn.forEach((btn, index) => {
   btn.addEventListener('click', () => {
-      printModal(`<h2 class="modal-title">${modalProject[index].title}</h2>
+    printModal(`<h2 class="modal-title">${modalProject[index].title}</h2>
       <i id ="close-menu" class="fas fa-times"></i>
       <ul class="modal-mark">
         <li>
@@ -166,11 +165,11 @@ projectBtn.forEach((btn,index) => {
         </div>
       </div>`);
 
-  const modalContainer = document.querySelector('#modal-container')
-  const modalRemover = document.querySelector('#close-menu');
-  modalRemover.addEventListener('click', () => {
-    modalContainer.remove();
-});
+    const modalContainer = document.querySelector('#modal-container');
+    const modalRemover = document.querySelector('#close-menu');
+    modalRemover.addEventListener('click', () => {
+      modalContainer.remove();
+    });
   });
 });
 mobilMenu();
