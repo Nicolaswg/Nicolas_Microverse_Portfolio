63b32bb1497ddf165808b0b444fad3a8be73cb13
Nigonbol123@gmail.com <nigonbol123@gmail.com>
2021-12-16 17:45:33 -0400

    Add local storage data

--- main.js
+++ main.js
@@ -202,4 +202,35 @@ projectBtn.forEach((btn, index) => {
     });
   });
 });
+
+function storageAvailable(type) {
+  let storage;
+  try {
+    storage = window[type];
+    const x = '__storage_test__';
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return e instanceof DOMException && (e.code === 22
+      || e.code === 1014 || e.name === 'QuotaExceededError'
+      || e.name === 'NS_ERROR_DOM_QUOTA_REACHED')
+      && (storage && storage.length !== 0);
+  }
+}
+
+if (storageAvailable('localStorage')) {
+  const input = [form.fullname, form.email, form.message];
+  input.forEach((input) => {
+    input.addEventListener('submit', () => {
+      const objData = {
+        fullname: form.fullname.value,
+        email: form.email.value,
+        message: form.message.value,
+      };
+      localStorage.setItem('data', JSON.stringify(objData));
+    });
+  });
+}
+
 mobilMenu();
